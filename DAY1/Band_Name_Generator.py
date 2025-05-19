31a1a983b743e680247a5e3f2a4c7ddefdddc399
Rishendra Ruppa <rishendra1337@gmail.com>
2025-05-19 18:21:47 +0530

    Commit

---
+++ DAY1/Band_Name_Generator.py
@@ -0,0 +1,6 @@
+#Band Name Generator
+print("Welcome to the band name generator!")
+str1 = input("What's the name of the city you grew up in?")
+str2 = input("What's the name of your pet?")
+str3 = str1 + " " + str2
+print("Your band name could be",str3)
