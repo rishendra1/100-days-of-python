3151fb8da57fefe6f978f595e8f4f0af0cb4aff8
Rishendra Ruppa <rishendra1337@gmail.com>
2025-05-19 18:05:54 +0530

    Commit

---
+++ Day2/Band_Name_Generator.py
@@ -0,0 +1,4 @@
+str1 = input("Enter the name of the city in which you grew up?")
+str2 = input("Enter the name of your pet?")
+str3 = str1 + " " + str2
+print(str3)
---
+++ Day2/Type.py
@@ -0,0 +1,25 @@
+'''print(type(123))
+print(type("hello"))
+print(type(123.122))
+print(type(167_234_456))
+print(type(True))
+print(type(False))
+print(type(None))'''
+#type casting
+#print(int("123") + int("123456"))
+'''
+Python Primitive Data Types
+1.int
+2.float
+3.str
+4.bool
+
+Python arithmetical Operators
++ - Addition
+- - Subtraction
+* - Multiplication
+/ - Float Division
+// - Integral part of the division
+** - Power - Exponentiation
+round(Number , no_of_decimal_digits)
+'''
