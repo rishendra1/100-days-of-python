2d7e58e57d435e66f0ef10836fe9015d797d5cb4
Rishendra Ruppa <rishendra1337@gmail.com>
2025-05-19 09:49:30 +0530

    Create Area_Volume_of_Cylinder.py

---
+++ DAY1/Area_Volume_of_Cylinder.py
@@ -0,0 +1,7 @@
+#Areas of Closed 3d figures
+#Cylinder
+radius = int(input("Enter radius of cylinder: "))
+height = int(input("Enter height of cylinder: "))
+print("c.s.a of cylinder is -", 2*3.14*radius*height)
+print("t.s.a of cylinder is -", 2*3.14*radius*(radius + height))
+print("Volume is -", 3.14 * radius *radius * height)
