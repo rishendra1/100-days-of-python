1f4015eab035ea2916d1818fc998ad4b0c4f6e31
Rishendra Ruppa <rishendra1337@gmail.com>
2025-05-19 09:30:49 +0530

    Create Areas_of_2d_figures.py

---
+++ DAY1/Areas_of_2d_figures.py
@@ -0,0 +1,15 @@
+#Areas of Closed 2d figures
+#Rectangle
+length = int(input("Enter length of rectangle: "))
+breadth = int(input("Enter breadth of rectangle: "))
+print("Area of Rectangle is -", length*breadth)
+#Square
+side = int(input("Enter length of side of a square: "))
+print("Area of square is -", side*side)
+#Circle
+radius = int(input("Enter radius of circle: "))
+print("Area of circle is -", 3.14 * radius * radius)
+#Triangle
+base = int(input("Enter base of Triangle: "))
+height = int(input("Enter height of Triangle: "))
+print("Area of Triangle is -", base*height/2)
